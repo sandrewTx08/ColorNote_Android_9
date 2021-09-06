b014618c436a82d427158c373e86e9c017083506
sandrewTx08 <sandrewtx08@protonmail.com>
2021-09-06 15:26:21 -0300

    add "Do tutorial" scenario

---
+++ features/do_tutorial.feature
@@ -0,0 +1,40 @@
+Feature: Do tutorial
+  User perform the tutorial
+
+  Scenario: Creating text
+    Given first app running
+    When "Welcome to ColorNote" show up
+    Then tap on right arrow button
+    And tap to create note button
+    And tap add text note type
+    And type some text
+    And press tree back button to close keyboard and save
+
+  Scenario: Creating checkbox
+    Given user continue the tutorial
+    When "Create a checklist" show up
+    Then tap create a note button
+    And tap add checklist
+    And tap at title textbox
+    And tap "Add item"
+    And tap "Add item" textbox
+    And tap "Next"
+    And tap "Add item" textbox
+    And tap "OK"
+    And press twice back button to save
+
+  Scenario: Final step
+    Given user continue the tutorial
+    When "Open the saved note" show up
+    Then tap on last added note
+    And on Pick up laundry
+    And tap edit the note
+    And tap on change note color button
+    And choose color red
+    And press back button to save
+    And tap on editing note
+    And tap pin status bar (dropdown menu)
+    And tap reminder
+    And tap "Pin to status bar"
+    And twice back button to save
+
---
+++ features/steps/do_tutorial.py
@@ -0,0 +1,140 @@
+from behave import *
+
+@when(u'"Welcome to ColorNote" show up')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When "Welcome to ColorNote" show up')
+
+
+@then(u'tap on right arrow button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap on right arrow button')
+
+
+@then(u'tap to create note button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap to create note button')
+
+
+@then(u'tap add text note type')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap add text note type')
+
+
+@then(u'type some text')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then type some text')
+
+
+@then(u'press tree back button to close keyboard and save')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then press tree back button to close keyboard and save')
+
+
+@given(u'user continue the tutorial')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given user continue the tutorial')
+
+
+@when(u'"Create a checklist" show up')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When "Create a checklist" show up')
+
+
+@then(u'tap create a note button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap create a note button')
+
+
+@then(u'tap add checklist')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap add checklist')
+
+
+@then(u'tap at title textbox')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap at title textbox')
+
+
+@then(u'tap "Add item"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap "Add item"')
+
+
+@then(u'tap "Add item" textbox')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap "Add item" textbox')
+
+
+@then(u'tap "Next"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap "Next"')
+
+
+@then(u'tap "OK"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap "OK"')
+
+
+@then(u'press twice back button to save')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then press twice back button to save')
+
+
+@when(u'"Open the saved note" show up')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When "Open the saved note" show up')
+
+
+@then(u'tap on last added note')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap on last added note')
+
+
+@then(u'on Pick up laundry')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then on Pick up laundry')
+
+
+@then(u'tap edit the note')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap edit the note')
+
+
+@then(u'tap on change note color button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap on change note color button')
+
+
+@then(u'choose color red')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then choose color red')
+
+
+@then(u'press back button to save')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then press back button to save')
+
+
+@then(u'tap on editing note')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap on editing note')
+
+
+@then(u'tap pin status bar (dropdown menu)')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap pin status bar (dropdown menu)')
+
+
+@then(u'tap reminder')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap reminder')
+
+
+@then(u'tap "Pin to status bar"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then tap "Pin to status bar"')
+
+
+@then(u'twice back button to save')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then twice back button to save')
